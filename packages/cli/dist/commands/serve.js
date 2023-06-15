d26fb17c8cb339dff1a99e97659e300788a974c3
Andriy Bas <andriy.bas@uptech.team>
2023-06-15 16:29:12 +0300

    cli

--- packages/cli/dist/commands/serve.js
+++ packages/cli/dist/commands/serve.js
@@ -1,4 +1,13 @@
 "use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
@@ -11,11 +20,27 @@ exports.serveCommand = new commander_1.Command()
     .command("serve [filename]")
     .description("Open a file for editing")
     .option("-p, --port <number>", "port to run server on", "4005")
-    .action((filename = "notebook.js", options) => {
+    .action((filename = "notebook.js", options) => __awaiter(void 0, void 0, void 0, function* () {
     const port = parseInt(options.port);
-    const dir = path_1.default.join(process.cwd(), path_1.default.dirname(filename));
-    const baseFilename = path_1.default.basename(filename);
-    // console.log(dir);
-    // console.log(baseFilename);
-    (0, local_api_1.serve)(port, baseFilename, dir);
-});
+    if (isNaN(port) || port < 0 || port >= 65536) {
+        console.log("port must be a number and should be >= 0 and < 65536");
+        return;
+    }
+    try {
+        const dir = path_1.default.join(process.cwd(), path_1.default.dirname(filename));
+        const baseFilename = path_1.default.basename(filename);
+        // console.log(dir);
+        // console.log(baseFilename);
+        yield (0, local_api_1.serve)(port, baseFilename, dir);
+        console.log(`Opened ${filename}. Navigate to http://localhost:${port}`);
+    }
+    catch (error) {
+        if (error.code === "EADDRINUSE") {
+            console.log(`Port ${port} is in use. Try running on a different port.`);
+        }
+        else {
+            console.log("Error: ", error.message);
+        }
+        process.exit(1);
+    }
+}));
